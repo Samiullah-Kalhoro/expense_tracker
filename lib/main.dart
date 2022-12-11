fb623484f13f94b5865b7ca7ea36c18b295128dd
Samiullah-Kalhoro <kalhorosami5@gmail.com>
2022-12-11 21:04:03 +0500

    Restructured and added functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expense_tracker/transaction.dart';
-import 'package:intl/intl.dart';
+import 'package:personal_expense_tracker/widgets/user_transactions.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -17,33 +17,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
+  const MyHomePage({super.key});
 
-  final amountController = TextEditingController();
+  // final amountController = TextEditingController();
   // String titleInput = "";
   // String amountInput = "";
-  final titleController = TextEditingController();
-
-  final List<Transaction> transactions = [
-    Transaction(
-      id: "t1",
-      title: "New Shoes",
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "t1",
-      title: "Weekly Groceries",
-      amount: 16.99,
-      date: DateTime.now(),
-    ),
-  ];
+  // final titleController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -55,10 +40,10 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
+        children: const [
+          SizedBox(
             width: double.infinity,
-            child: const Card(
+            child: Card(
               color: Colors.yellow,
               elevation: 5,
               child: Text(
@@ -66,81 +51,7 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          Card(
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextFormField(
-                    controller: titleController,
-                    decoration: const InputDecoration(labelText: "Title"),
-                    // onChanged: (value) => titleInput = value,
-                  ),
-                  TextFormField(
-                    controller: amountController,
-                    decoration: const InputDecoration(labelText: "Amount"),
-                    // onChanged: (value) => amountInput = value,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      print(titleController.text);
-                      print(amountController.text);
-                    },
-                    child: const Text("Add Transaction"),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      "\$${tx.amount}",
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: const TextStyle(
-                          fontSize: 11.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  )
-                ]),
-              );
-            }).toList(),
-          ),
+          UserTransactions(),
         ],
       ),
     );
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:personal_expense_tracker/widgets/user_transactions.dart';
+
+class NewTransaction extends StatelessWidget {
+  NewTransaction(this.addTx, {super.key});
+
+  final amountController = TextEditingController();
+  final titleController = TextEditingController();
+  final Function addTx;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextFormField(
+              controller: titleController,
+              decoration: const InputDecoration(labelText: "Title"),
+              // onChanged: (value) => titleInput = value,
+            ),
+            TextFormField(
+              controller: amountController,
+              decoration: const InputDecoration(labelText: "Amount"),
+              // onChanged: (value) => amountInput = value,
+            ),
+            TextButton(
+              onPressed: () {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+              child: const Text("Add Transaction"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
