18215b530d52a4700746c5c89b657466d57269c3
Samiullah-Kalhoro <kalhorosami5@gmail.com>
2022-12-14 17:27:27 +0500

    bars designed

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -22,21 +22,23 @@ class Chart extends StatelessWidget {
           totalSum += recentTransaction[i].amount;
         }
       }
-      print(DateFormat.E().format(weekDay));
-      print(totalSum);
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']} : ${data['amount']}');
+        }).toList(),
       ),
     );
   }
